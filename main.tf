d1358bdc9383a22bf7f22601b77c4c38a1dbae72
srizvi0 <35908177+srizvi0@users.noreply.github.com>
2024-02-06 18:23:23 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     azurerm = {
-      version = "3.0.0"
       source  = "hashicorp/azurerm"
+      version = "3.0.0"
     }
   }
 }
@@ -11,48 +11,48 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "RG1" {
+resource "azurerm_resource_group" "resource_group" {
   name     = "Resource_Group_1"
   location = var.location
 }
 
-resource "azurerm_virtual_network" "VNET1" {
+resource "azurerm_virtual_network" "virtual_network" {
   name                = "Virtual_Network_1"
-  location            = azurerm_resource_group.RG1.location
-  resource_group_name = azurerm_resource_group.RG1.name
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
   address_space       = ["10.0.0.0/16"]
 }
 
-resource "azurerm_subnet" "SUB1" {
-  resource_group_name    = azurerm_resource_group.RG1.name
+resource "azurerm_subnet" "subnet" {
   name                   = "subnet_1"
-  virtual_network_name   = azurerm_virtual_network.VNET1.name
-  depends_on             = [azurerm_virtual_network.VNET1]
+  resource_group_name    = azurerm_resource_group.resource_group.name
+  virtual_network_name   = azurerm_virtual_network.virtual_network.name
+  depends_on             = [azurerm_virtual_network.virtual_network]
   address_prefixes       = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "NIC1" {
-  name                      = "network_interface1"
-  resource_group_name       = azurerm_resource_group.RG1.name
-  location                  = azurerm_resource_group.RG1.location
-  depends_on                = [azurerm_virtual_network.VNET1]
+resource "azurerm_network_interface" "network_interface" {
+  name                      = "network_interface_1"
+  resource_group_name       = azurerm_resource_group.resource_group.name
+  location                  = azurerm_resource_group.resource_group.location
+  depends_on                = [azurerm_virtual_network.virtual_network]
 
   ip_configuration {
-    name                          = "ip_configuration1"
+    name                          = "ip_configuration_1"
     private_ip_address_allocation = "Dynamic"
-    subnet_id                     = azurerm_subnet.SUB1.id
+    subnet_id                     = azurerm_subnet.subnet.id
   }
 }
 
-resource "azurerm_windows_virtual_machine" "VM1" {
-  name                = "VM-1"
-  resource_group_name = azurerm_resource_group.RG1.name
-  location            = azurerm_resource_group.RG1.location
+resource "azurerm_windows_virtual_machine" "virtual_machine" {
+  name                = "vm-1"
+  resource_group_name = azurerm_resource_group.resource_group.name
+  location            = azurerm_resource_group.resource_group.location
   size                = "Standard_F2"
   admin_username      = "VirtualMachine1"
   admin_password      = "VirtualMachinePass1"
   network_interface_ids = [
-    azurerm_network_interface.NIC1.id,
+    azurerm_network_interface.network_interface.id,
   ]
   os_disk {
     storage_account_type = "Standard_LRS"
@@ -66,14 +66,14 @@ resource "azurerm_windows_virtual_machine" "VM1" {
   }
 }
 
-resource "azurerm_kubernetes_cluster" "AKS1" {
-  name                = "AKS-1"
+resource "azurerm_kubernetes_cluster" "kubernetes_cluster" {
+  name                = "Cluster_1"
+  resource_group_name = azurerm_resource_group.resource_group.name
   location            = "East US"
-  resource_group_name = azurerm_resource_group.RG1.name
   dns_prefix          = "DNSAKS-1"
 
   default_node_pool {
-    name       = "nodepool1"
+    name       = "defaultnp"
     vm_size    = "Standard_F2"
     node_count = 1
   }
@@ -83,10 +83,10 @@ resource "azurerm_kubernetes_cluster" "AKS1" {
   }
 }
 
-resource "azurerm_container_registry" "ACR1" {
-  name                = "ACRNEW125"
+resource "azurerm_container_registry" "container_registry" {
+  name                = "ACRNew12"
   location            = "East US"
-  resource_group_name = azurerm_resource_group.RG1.name
+  resource_group_name = azurerm_resource_group.resource_group.name
   sku                 = "Basic"
 
   identity {
@@ -96,24 +96,23 @@ resource "azurerm_container_registry" "ACR1" {
 }
 
 resource "azurerm_user_assigned_identity" "Identity1" {
-  name                = "AUSI"
-  resource_group_name = azurerm_resource_group.RG1.name
+  name                = "Identity_1"
+  resource_group_name = azurerm_resource_group.resource_group.name
   location            = "East US"
 }
 
 # Outputs for terraform resources
 
-
 output "virtual_machine_public_ip" {
-  value = azurerm_windows_virtual_machine.VM1.public_ip_address
+  value = azurerm_windows_virtual_machine.virtual_machine.public_ip_address
 }
 
 output "kubernetes_cluster_id" {
-  value = azurerm_kubernetes_cluster.AKS1.id
+  value = azurerm_kubernetes_cluster.kubernetes_cluster.id
 }
 
 output "container_registry_login_server" {
-  value = azurerm_container_registry.ACR1.login_server
+  value = azurerm_container_registry.container_registry.login_server
 }
 
 output "user_assigned_identity_principal_id" {
